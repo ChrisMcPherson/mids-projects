a07d36e0f43ca8ed2d69f7e8b41abca0af83f400
ChrisMcPherson <summernightdrive@gmail.com>
2016-06-16 21:30:13 -0400

    Updated bash script and added hive table create script

---
+++ w205_exercise_1/loading_and_modelling/hive_base_ddl.sql
@@ -0,0 +1,92 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS procedures 
+(	hospital_id int,
+	hospital_name string,
+	address string,
+	city string,
+	state string,
+	zip string,
+	county string,
+	phone string,
+	condition string,
+	measure_id int,
+	measure_name string,
+	score int,
+	sample int,
+	footnote string,
+	measure_st_dt string,
+	measure_end_dt string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+ "separatorChar" = ",",
+ "quoteChar" = '"',
+ "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/procedures';
+
+CREATE EXTERNAL TABLE IF NOT EXISTS survey_responses
+(	hospital_id int,
+	hospital_name string,
+	address string,
+	city string,
+	state string,
+	zip string,
+	county string,
+	nurse_comm_score string,
+	q2 string,
+	q3 string,
+	doc_comm_score string,
+	q5 string,
+	q6 string,
+	hos_rsp_score string,
+	q8 string,
+	q9 string,
+	pain_mgmt_score string,
+	q11 string,
+	q12 string,
+	med_comm_score string,
+	q14 string,
+	q15 string,
+	hos_cln_q_score string,
+	q17 string,
+	q18 string,
+	dis_comm_score string,
+	q20 string,
+	q21 string,
+	hos_over_all_score string,
+	q23 string,
+	q24 string,
+	hcahps_base_score int,
+	hcahps_cons_score int
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+ "separatorChar" = ",",
+ "quoteChar" = '"',
+ "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/surveys';
+
+CREATE EXTERNAL TABLE IF NOT EXISTS hospitals 
+(	hospital_id int,
+	hospital_name string,
+	address string,
+	city string,
+	state string,
+	zip int,
+	county string,
+	phone string,
+	hospital_type string,
+	ownerwhip string,
+	emergency_services string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+ "separatorChar" = ",",
+ "quoteChar" = '"',
+ "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/hospitals';
--- w205_exercise_1/loading_and_modelling/load_data_lake.sh
+++ w205_exercise_1/loading_and_modelling/load_data_lake.sh
@@ -1,6 +1,6 @@
 su - w205
 mkdir -p /home/w205/exercise_1_data
-cd exercise_1_data
+cd /home/w205/exercise_1_data
 
 mv "Measure Dates.csv" measures.csv
 mv "Hospital General Information.csv" hospitals.csv
@@ -8,10 +8,15 @@ mv "Timely and Effective Care - Hospital.csv" effective_care_hospital.csv
 mv "Readmissions and Deaths - Hospital.csv" readmissions_hospital.csv
 mv "hvbp_hcahps_05_28_2015.csv" survey_responses.csv
 
-hdfs dfs -mkdir /user/w205/hospital_compare/
+hdfs dfs -mkdir /user/w205/hospital_compare
+hdfs dfs -mkdir /user/w205/hospital_compare/measures
+hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir /user/w205/hospital_compare/surveys
+hdfs dfs -mkdir /user/w205/hospital_compare/procedures
 
-hdfs dfs -put measures.csv /user/w205/hospital_compare
-hdfs dfs -put hospitals.csv /user/w205/hospital_compare
-hdfs dfs -put readmissions_hospital.csv /user/w205/hospital_compare
-hdfs dfs -put survey_responses.csv /user/w205/hospital_compare
-hdfs dfs -put effective_care_hospital.csv /user/w205/hospital_compare
+hdfs dfs -put measures.csv /user/w205/hospital_compare/measures
+hdfs dfs -put hospitals.csv /user/w205/hospital_compare/hospitals
+hdfs dfs -put readmissions_hospital.csv /user/w205/hospital_compare/readmissions
+hdfs dfs -put survey_responses.csv /user/w205/hospital_compare/surveys
+hdfs dfs -put effective_care_hospital.csv /user/w205/hospital_compare/procedures
